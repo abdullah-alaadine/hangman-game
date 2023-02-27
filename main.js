08deb16403b3796c2210824f77bcb055df53999b
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-02-27 22:50:00 +0200

    sixth attempt function was added

--- main.js
+++ main.js
@@ -180,9 +180,16 @@ const attempts = {
         c.lineTo(200, 220);
         c.stroke();
     },
+    sixthAttempt: () => {
+        c.beginPath();
+        c.moveTo(200, 160);
+        c.lineTo(240, 140);
+        c.stroke();
+    },
 }
 attempts.firstAttempt()
 attempts.secondAttempt()
 attempts.thirdAttempt()
 attempts.fourthAttempt();
 attempts.fifthAttempt()
+attempts.sixthAttempt()
